0e6e645b60f91ae0bfe8c79880020610fbba8b25
たわ <tawa@tawanoMacBook-Pro.local>
2024-01-14 12:54:50 +0900

    add s3-1

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,5 @@
 require_relative "boot"
-
 require "rails/all"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,22 +7,8 @@ Bundler.require(*Rails.groups)
 module MtApp
   class Application < Rails::Application
     config.i18n.default_locale = :ja  # デフォルトのローカルを日本語に設定
-
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
     config.active_record.sqlite3_production_warning = false  #sqliteへの警告を無効
   end
 end
